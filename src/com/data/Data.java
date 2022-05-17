0141377e528101923dec9d9b0e51888e7dee8fea
Piotr Wilczyński <wiluplwolf@gmail.com>
2022-05-17 19:11:49 +0200

    Update

--- src/com/data/Data.java
+++ src/com/data/Data.java
@@ -2,7 +2,7 @@ package com.data;
 
 public class Data {
     public static void main(String[] args) {
-        Data5 dt = new Data5();
+        Data6 dt = new Data6();
         dt.data();
     }
 }
--- src/com/exercises/Exercise.java
+++ src/com/exercises/Exercise.java
@@ -2,7 +2,7 @@ package com.exercises;
 
 public class Exercise {
     public static void main(String[] args) {
-        Exercise13 ex = new Exercise13();
+        Exercise25 ex = new Exercise25();
         ex.exercise();
     }
 }
